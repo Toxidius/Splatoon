43c59fedf0e4fbf6942efcc882e7f3bbec1fefa6
Toxidius <toxidium@hotmail.com>
2017-05-05 02:17:05 -0500

    Add files via upload

--- GameMechanics/SplatChargerRunnable.java
+++ GameMechanics/SplatChargerRunnable.java
@@ -10,32 +10,21 @@ import Splatoon.Main.Core;
 
 public class SplatChargerRunnable implements Runnable{
 
+	private int maximumArrows = 3;
 	public int id;
 	
 	public SplatChargerRunnable() {
 		id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.thisPlugin, this, 80, 80); // every 4 seconds give arrow
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Override
 	public void run() {
 		for (Player player : Bukkit.getOnlinePlayers()){
 			if (player.getGameMode() == GameMode.SURVIVAL){
 				int kit = Core.gameManager.getPlayerKit(player);
 				if (kit == 2){
-					int team = Core.gameManager.getPlayerTeam(player);
-					byte woolColor = Core.gameManager.getPlayerWoolColor(player);
-					if (team == 1
-							&& player.getLocation().add(0.0, -1.0, 0.0).getBlock().getType() == Material.WOOL
-							&& player.getLocation().add(0.0, -1.0, 0.0).getBlock().getData() == woolColor){
-						player.getInventory().addItem(new ItemStack(Material.ARROW, 2));
-					}
-					else if (team == 2
-							&& player.getLocation().add(0.0, -1.0, 0.0).getBlock().getType() == Material.WOOL
-							&& player.getLocation().add(0.0, -1.0, 0.0).getBlock().getData() == woolColor){
-						player.getInventory().addItem(new ItemStack(Material.ARROW, 2));
-					}
-					else{
+					int currentAmount = Core.gameManager.getAmountInPlayerInventory(player, Material.ARROW);
+					if (currentAmount < maximumArrows){
 						player.getInventory().addItem(new ItemStack(Material.ARROW, 1));
 					}
 				}
